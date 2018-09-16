12ca96b4e038d68d14f4602b878482e8908f2f98
tigerfansga <24516562+tigerfansga@users.noreply.github.com>
2018-09-15 23:29:19 -0400

    Add Remove-GogsUser

---
+++ GogsPS/Public/Remove-GogsUser.ps1
@@ -0,0 +1,42 @@
+function Remove-GogsUser {
+    [CmdletBinding(DefaultParameterSetName = 'Credential')]
+    param (
+        # Parameter help description
+        [Parameter(Mandatory = $true, HelpMessage = 'Base Uri of Gogs Server')]
+        [String] $BaseUri,
+
+        [Parameter(ParameterSetName = 'Credential', Mandatory = $False)]
+        [pscredential] $Credential,
+
+        # Parameter help description
+        [Parameter(ParameterSetName = 'Token', Mandatory = $False)]
+        [securestring] $Token,
+        
+        # Parameter help description
+        [Parameter()]
+        [switch] $AllowUnencryptedAuthentication = $false,
+
+        [Parameter(Mandatory = $true,
+            HelpMessage = 'Username to delete')]
+        [string] $UserName
+    )
+    
+    begin {
+    }
+    
+    process {
+        $Parms = @{ BaseUri = $BaseUri; ApiEndpoint = "/admin/users/$UserName"; Method = 'Delete'; ApiVersion = '1' }
+        Foreach ($var in "Credential", "Token", "AllowUnencryptedAuthentication") {
+            $val = Get-Variable -Name $var -ValueOnly -ErrorAction SilentlyContinue
+            if ($val) {
+                $Parms[$var] = $val
+            }
+        }
+
+        $user = Invoke-GogsApi @Parms
+        return $user
+    }
+    
+    end {
+    }
+}
